46b520823ddd87338f2100d204b97d5989e4c312
Kerzhakov Nikita Alexeevich <nikitakerzhik@gmail.com>
2024-05-14 21:56:43 +1000

    scaner v2

--- main.go
+++ main.go
@@ -3,18 +3,27 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
-func main() {
-	for port := 1; port < 1024; port++ {
-		addr := fmt.Sprintf("scanme.nmap.org:%d", port)
-		conn, err := net.Dial("tcp", addr)
+func isPortOpened(port int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	addr := fmt.Sprintf("scanme.nmap.org:%d", port)
+	conn, err := net.Dial("tcp", addr)
 
-		if err != nil {
-			continue
-		}
-
-		conn.Close()
-		fmt.Printf("Port %d open\n", port)
+	if err != nil {
+		return
 	}
+
+	conn.Close()
+	fmt.Printf("Port %d open\n", port)
+}
+
+func main() {
+	var wg sync.WaitGroup
+	for port := 1; port < 1024; port++ {
+		wg.Add(1)
+		go isPortOpened(port, &wg)
+	}
+	wg.Wait()
 }
